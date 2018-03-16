f0356bd7ff3179536613bb9382793248da88bacf
zacanger <zac@zacanger.com>
2018-03-15 19:07:31 -0600

    Add some stuff

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # microfront
 
+[WIP]
+
+An idea for microservices on the front end
+
 --------
 
 ## Installation
 
+`npm i microfront`
+
 ## Usage
+
+```javascript
+import m from 'microfront'
+
+m('/manifest.json')
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-module.exports = {}
+const microApps = (manifest) => {
+  const get = async (url, opts) => {
+    const res = await window.fetch(url, opts)
+    if (!res.ok) throw new Error(res.statusText)
+    return res.json()
+  }
+
+  const each = async (xs, cb) => {
+    for (let i = 0; i < xs.length; i++) {
+      await cb(xs[i])
+    }
+  }
+
+  const run = async () => {
+    const apps = await get(manifest)
+    await each(apps, async (m) => {
+      const { mount, app } = await import(m)
+      mount(app)
+    })
+  }
+
+  run()
+}
