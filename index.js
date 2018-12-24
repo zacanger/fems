4f03c859d65a4089a02a9a46ee5a75211cd5d92c
zacanger <zac@zacanger.com>
2018-12-24 09:44:28 -0700

    Update deps; use shimport shim

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const load = (src) => {
+  try {
+    // eslint-disable-next-line
+    new Function('import("' + src + '")')()
+  } catch (e) {
+    const s = document.createElement('script')
+    s.src = 'https://unpkg.com/shimport'
+    s.dataset.main = src
+    document.head.appendChild(s)
+  }
+}
+
 const get = async (url, opts) => {
   const res = await window.fetch(url, opts)
   if (!res.ok) throw new Error(res.statusText)
@@ -13,7 +25,7 @@ const each = async (xs, cb) => {
 export default async (manifest) => {
   const apps = await get(manifest)
   await each(apps, async (m) => {
-    const { run } = await import(m)
+    const { run } = await load(m)
     run()
   })
 }
