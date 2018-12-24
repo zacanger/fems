b706052254bb04229445fc42d17c6cd767765536
zacanger <zac@zacanger.com>
2018-12-24 16:12:18 -0700

    try a thing

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+/* eslint-disable */
+function get (url, opts) {
+  window.fetch(url, opts)
+    .then(function (res) {
+      if (!res.ok) {
+        throw new Error(res.statusText)
+      }
+      return res.json()
+    })
+}
+
+function each (xs, cb) {
+  for (var i = 0; i < xs.length; i++) {
+    cb(xs[i]).then(function () {})
+  }
+}
+
+function load (src) {
+  try {
+    // eslint-disable-next-line
+    new Function('import("' + src + '")')()
+  } catch (e) {
+    var s = document.createElement('script')
+    s.src = 'https://unpkg.com/shimport'
+    s.dataset.main = src
+    document.head.appendChild(s)
+  }
+}
+
+function fems (manifest) {
+  get(manifest)
+    .then(function (apps) {
+      each(apps)
+        .then(function (m) {
+          load(m)
+            .then(function (n) {
+              n.run()
+            })
+        })
+    })
+}
+
+const fems = async (manifest) => {
+  const apps = await get(manifest)
+  await each(apps, async (m) => {
+    const { run } = await load(m)
+    run()
+  })
+}
+
+/*
 const load = (src) => {
   try {
     // eslint-disable-next-line
@@ -29,6 +80,17 @@ const fems = async (manifest) => {
     run()
   })
 }
+*/
 
-export default fems
-window.__fems = fems
+// UMD-ish + ES modules
+if (typeof module !== 'undefined' && typeof exports === 'object') {
+  Object.defineProperty(exports, '__esmodule', {
+    value: true
+  })
+  module.exports = fems
+  module.exports.default = fems
+} else if (typeof window !== 'undefined') {
+  window.__fems = fems
+} else if (typeof global !== 'undefined' && typeof global === 'object') {
+  global.fems = fems
+}
